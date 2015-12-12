6136b67fd5d0ca526567a222ab6254e51bc3cdf9
Piotr Kowalski <piet.kowalski@gmail.com>
2015-12-12 12:57:49 +0100

    Start with very Spartan chat interface

--- server.js
+++ server.js
@@ -1,48 +1,22 @@
-var mongo = require('mongodb').MongoClient;
-var client = require('socket.io').listen(8080).sockets;
-
-mongo.connect('mongodb://127.0.0.1/chat', function (err, db) {
-
-	if (err) {
-		throw err;
-	}
-
-	client.on('connection', function (socket) {
-
-		var collection = db.collection("messages");
-
-		var sendStatus = function (s) {
-			socket.emit('status', s);
-		};
-
-		collection.find().limit(100).sort({_id: 1}).toArray(function (err, res) {
-
-			if (err) {
-				throw err;
-			}
-			socket.emit('output', res);
-		});
-
-		socket.on("input", function (data) {
-
-			var pattern = /^\s*$/;
-			var name = data.name;
-			var message = data.message;
-
-			if (pattern.test(name) || pattern.test(message)) {
-				sendStatus('Name and message are required.');
-			} else {
-				collection.insert({
-					name: name,
-					message: message
-				}, function () {
-					client.emit('output', [data]);
-					sendStatus({
-						message: "Message sent.",
-						clear: true
-					});
-				});
-			}
-		});
-	});
+var httpd = require('http').createServer(handler);
+var io = require('socket.io').listen(httpd);
+var fs = require('fs');
+httpd.listen(8080);
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+            function (err, data) {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end('Error loading index.html');
+                }
+                res.writeHead(200);
+                res.end(data);
+            }
+    );
+}
+io.sockets.on('connection', function (socket) {
+    socket.on('clientMessage', function (content) {
+        socket.emit('serverMessage', 'You said: ' + content);
+        socket.broadcast.emit('serverMessage', socket.id + ' said: ' + content);
+    });
 });
